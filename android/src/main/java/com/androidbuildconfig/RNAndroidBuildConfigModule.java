5ee6b831aec3f6af76996b6279b7cf3d37246ed6
Shivam Aditya <sa.shivam.aditya@gmail.com>
2018-01-16 02:29:49 +0530

    changed made to create a workaround for rn android

--- android/src/main/java/com/androidbuildconfig/RNAndroidBuildConfigModule.java
+++ android/src/main/java/com/androidbuildconfig/RNAndroidBuildConfigModule.java
@@ -1,5 +1,6 @@
 package com.androidbuildconfig;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.facebook.react.bridge.Promise;
@@ -17,11 +18,14 @@ public class RNAndroidBuildConfigModule extends ReactContextBaseJavaModule {
     private Class buildConfigClass;
     private String NAME = "RNAndroidBuildConfig";
     private Map<String, Object> buildConfigConstants = new HashMap<>();
+    private String utmSource;
 
-    public RNAndroidBuildConfigModule(ReactApplicationContext reactContext, Class buildConfigClass) {
+    public RNAndroidBuildConfigModule(ReactApplicationContext reactContext, Class buildConfigClass, String UtmSource) {
         super(reactContext);
         this.reactContext = reactContext;
         this.buildConfigClass = buildConfigClass;
+        this.utmSource = UtmSource;
+        Log.d(NAME, "In constructor in RNAndroidBuildConfigModule. buildConfigClass size is: " + buildConfigClass.getDeclaredFields().length);
     }
 
     @Override
@@ -31,10 +35,18 @@ public class RNAndroidBuildConfigModule extends ReactContextBaseJavaModule {
 
     @Override
     public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = getStringObjectMap();
+        buildConfigConstants = constants;
+
+        return constants;
+    }
+
+    @NonNull
+    private Map<String, Object> getStringObjectMap() {
         final Map<String, Object> constants = new HashMap<>();
 
         Field[] fields = buildConfigClass.getDeclaredFields();
-        Log.d(NAME, "BuildConfig fields are: " + fields.toString());
+        Log.d(NAME, "BuildConfig size is: " + fields.length);
 
         for (Field f : fields) {
             try {
@@ -45,23 +57,55 @@ public class RNAndroidBuildConfigModule extends ReactContextBaseJavaModule {
                 Log.d(NAME, "Could not access BuildConfig field " + f.getName());
             }
         }
-
-        this.buildConfigConstants = constants;
-
         return constants;
     }
 
+    //working
     @ReactMethod
     public void getUtmSource(Promise promise) {
-        String utmSource = this.buildConfigConstants.get("DEFAULT_UTM_SOURCE").toString();
-        Log.d(NAME, "UtmSource from getUtmSource in RNAndroidBuildConfig is: " + utmSource);
-        promise.resolve(utmSource);
+        Log.d(NAME, "UtmSource from getUtmSource in RNAndroidBuildConfig is" + this.utmSource);
+        promise.resolve(this.utmSource);
     }
 
+    //non-working
+    @ReactMethod
+    public void getUtmSourceFromClass(Promise promise) {
+        if (this.buildConfigConstants == null || this.buildConfigConstants.isEmpty()) {
+            Log.d(NAME, "buildConfigConstants in RNAndroidBuildConfig is null");
+            final Map<String, Object> constants = getStringObjectMap();
+            buildConfigConstants = constants;
+        }
+
+        int size = buildConfigConstants.size();
+        Log.d(NAME, "buildConfigConstants size after setting from null in getUtmSource is" + size);
+        Log.d(NAME, "buildConfigConstants after setting from null in getUtmSource is" + buildConfigConstants);
+        if (size > 0) {
+            String utmSource = buildConfigConstants.get("DEFAULT_UTM_SOURCE").toString();
+            Log.d(NAME, "UtmSource from getUtmSource in getUtmSource is: " + utmSource);
+            promise.resolve(utmSource);
+        } else {
+            promise.resolve("NO DATA GOT");
+        }
+    }
+
+    //non-working
     @ReactMethod
     public void getBuildConfigValue(String key, Promise promise) {
-        Log.d(NAME, "getBuildConfigValue key is" + key);
-        String value = this.buildConfigConstants.get(key).toString();
-        promise.resolve(value);
+        if (this.buildConfigConstants == null || this.buildConfigConstants.isEmpty()) {
+            Log.d(NAME, "buildConfigConstants in RNAndroidBuildConfig is null");
+            final Map<String, Object> constants = getStringObjectMap();
+            buildConfigConstants = constants;
+        }
+
+        int size = buildConfigConstants.size();
+        Log.d(NAME, "buildConfigConstants size after setting from null in getBuildConfigValue is" + size);
+        Log.d(NAME, "buildConfigConstants after setting from null in getBuildConfigValue is" + buildConfigConstants);
+        if (size > 0) {
+            Log.d(NAME, "getBuildConfigValue key is" + key);
+            String value = buildConfigConstants.get(key).toString();
+            promise.resolve(value);
+        } else {
+            promise.resolve("NO DATA GOT2 ");
+        }
     }
 }
--- android/src/main/java/com/androidbuildconfig/RNAndroidBuildConfigPackage.java
+++ android/src/main/java/com/androidbuildconfig/RNAndroidBuildConfigPackage.java
@@ -1,9 +1,13 @@
 
 package com.androidbuildconfig;
 
+import android.util.Log;
+
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
@@ -13,14 +17,20 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class RNAndroidBuildConfigPackage implements ReactPackage {
 
     private Class buildConfigClass;
+    private String utmSource;
+    private String NAME = "RNAndroidBuildConfig";
+    private Map<String, Object> buildConfigConstants = new HashMap<>();
 
-    public RNAndroidBuildConfigPackage(Class buildConfigClass) {
+    public RNAndroidBuildConfigPackage(Class buildConfigClass, String UtmSource) {
+        Log.d(NAME, "UtmSource at start in RNAndroidBuildConfigPackage is" + UtmSource);
+        this.utmSource = UtmSource;
+        Log.d(NAME, "In RNAndroidBuildConfigPackage constructor. buildConfigClass size is: " + buildConfigClass.getDeclaredFields().length);
         this.buildConfigClass = buildConfigClass;
     }
 
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNAndroidBuildConfigModule(reactContext, this.buildConfigClass));
+      return Arrays.<NativeModule>asList(new RNAndroidBuildConfigModule(reactContext, this.buildConfigClass, this.utmSource));
     }
 
     // Deprecated from RN 0.47
